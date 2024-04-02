37128419c0253c1d30a3cbea89fe6fe3646c4f55
unknown <lukacc008@gmail.com>
2024-04-02 17:23:04 +0200

    Linking the Context to State, and using functions through Context aswell

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -3,10 +3,11 @@ import { useContext } from 'react';
 import { CartContext } from "../store/shopping-cart-context";
 
 export default function Cart({ onUpdateItemQuantity }) {
-  const cartCtx = useContext(CartContext);
+  const { items } = useContext(CartContext); //destructure items from CartContext so we dont
+  //have to use cartCtx.items.lenght etc... and use just items.length
 
 
-  const totalPrice = cartCtx.items.reduce(
+  const totalPrice = items.reduce(
     (acc, item) => acc + item.price * item.quantity,
     0
   );
@@ -14,10 +15,10 @@ export default function Cart({ onUpdateItemQuantity }) {
 
   return (
     <div id="cart">
-      {cartCtx.items.length === 0 && <p>No items in cart!</p>}
-      {cartCtx.items.length > 0 && (
+      {items.length === 0 && <p>No items in cart!</p>}
+      {items.length > 0 && (
         <ul id="cart-items">
-          {cartCtx.items.map((item) => {
+          {items.map((item) => {
             const formattedPrice = `$${item.price.toFixed(2)}`;
 
             return (
